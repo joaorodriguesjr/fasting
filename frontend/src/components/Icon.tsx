ca2a326b792b903df42e7325b8f402681ac07318
joaorodriguesjr <mail@joaorodriguesjr.com>
2022-06-10 13:28:35 -0300

    Add static elements

--- frontend/src/components/Icon.tsx
+++ frontend/src/components/Icon.tsx
@@ -2,6 +2,12 @@ import { FunctionComponent } from 'react'
 
 const svgs = new Map([
   [
+    'Edit',
+    <svg key={'Edit'} xmlns="http://www.w3.org/2000/svg" version="1.1" width="24" height="24" viewBox="0 0 24 24">
+       <path fill="currentColor" d="M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12H20A8,8 0 0,1 12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4V2M18.78,3C18.61,3 18.43,3.07 18.3,3.2L17.08,4.41L19.58,6.91L20.8,5.7C21.06,5.44 21.06,5 20.8,4.75L19.25,3.2C19.12,3.07 18.95,3 18.78,3M16.37,5.12L9,12.5V15H11.5L18.87,7.62L16.37,5.12Z" />
+    </svg>
+  ],
+  [
     'Wrench',
     <svg key={'Wrench'} xmlns="http://www.w3.org/2000/svg" version="1.1" width="24" height="24" viewBox="0 0 24 24">
        <path fill="currentColor" d="M22.7,19L13.6,9.9C14.5,7.6 14,4.9 12.1,3C10.1,1 7.1,0.6 4.7,1.7L9,6L6,9L1.6,4.7C0.4,7.1 0.9,10.1 2.9,12.1C4.8,14 7.5,14.5 9.8,13.6L18.9,22.7C19.3,23.1 19.9,23.1 20.3,22.7L22.6,20.4C23.1,20 23.1,19.3 22.7,19Z" />
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -26,6 +26,29 @@ const Home: NextPage = () => {
     {fasts.target}/{24 - fasts.target} <Icon name={'Cog'}/>
   </div>
 
+  const status = <div className={styles.status}>
+    <div className={styles.status__line}>
+      <svg width="8" height="40" viewBox="0 0 8 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M8 4C8 6.20914 6.20914 8 4 8C1.79086 8 0 6.20914 0 4C0 1.79086 1.79086 0 4 0C6.20914 0 8 1.79086 8 4ZM0.8 4C0.8 5.76731 2.23269 7.2 4 7.2C5.76731 7.2 7.2 5.76731 7.2 4C7.2 2.23269 5.76731 0.8 4 0.8C2.23269 0.8 0.8 2.23269 0.8 4Z" fill="#7F7F7F"/>
+        <path d="M8 36C8 38.2091 6.20914 40 4 40C1.79086 40 0 38.2091 0 36C0 33.7909 1.79086 32 4 32C6.20914 32 8 33.7909 8 36ZM0.8 36C0.8 37.7673 2.23269 39.2 4 39.2C5.76731 39.2 7.2 37.7673 7.2 36C7.2 34.2327 5.76731 32.8 4 32.8C2.23269 32.8 0.8 34.2327 0.8 36Z" fill="#FFD000"/>
+        <line x1="4" y1="8" x2="4" y2="32" stroke="#595959" strokeWidth="2" strokeDasharray="4 4"/>
+      </svg>
+    </div>
+    <table>
+      <tbody>
+        <tr>
+          <th>Started</th>
+          <td className={styles.highlight}>Today, 12:58 pm</td>
+          <td><Icon name={'Edit'}/></td>
+        </tr>
+        <tr>
+          <th>Target</th>
+          <td colSpan={2}>Tomorrow, 04:58 am</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
   const progress = (fasts.current == null)
     ? <>
         <Progress start={0} target={0}>
@@ -43,12 +66,47 @@ const Home: NextPage = () => {
             <Stopwatch activation={ fasts.current.start } />
           </div>
         </Progress>
+        {status}
         <button onClick={stopFasting}>End Fast</button>
       </>
 
   return <>
     <div className={styles.container}>
       {progress}
+
+      <div className={styles.report}>
+        <div className={styles.report_month}>January 2022</div>
+        <div className={styles.report_days}>
+          <div className={styles.report_day}>
+            <div className={styles.report_number}>15</div>
+            <div className={styles.report_hours}></div>
+          </div>
+          <div className={styles.report_day}>
+            <div className={styles.report_number}>16</div>
+            <div className={styles.report_hours}></div>
+          </div>
+          <div className={styles.report_day}>
+            <div className={styles.report_number}>17</div>
+            <div className={styles.report_hours}></div>
+          </div>
+          <div className={styles.report_day}>
+            <div className={styles.report_number}>18</div>
+            <div className={styles.report_hours}></div>
+          </div>
+          <div className={styles.report_day}>
+            <div className={styles.report_number}>19</div>
+            <div className={styles.report_hours}></div>
+          </div>
+          <div className={styles.report_day}>
+            <div className={styles.report_number}>20</div>
+            <div className={styles.report_hours}></div>
+          </div>
+          <div className={styles.report_day}>
+            <div className={styles.report_number}>21</div>
+            <div className={styles.report_hours}></div>
+          </div>
+        </div>
+      </div>
     </div>
   </>
 }
