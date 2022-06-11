22e8280fa97d9baa94875e3ff3b918adf756ae5a
joaorodriguesjr <mail@joaorodriguesjr.com>
2022-06-11 13:44:48 -0300

    Add chevron icons

--- frontend/src/components/Icon.tsx
+++ frontend/src/components/Icon.tsx
@@ -37,6 +37,18 @@ const svgs = new Map([
       <path fill="currentColor" d="M20,11V13H8L13.5,18.5L12.08,19.92L4.16,12L12.08,4.08L13.5,5.5L8,11H20Z" />
     </svg>
   ],
+  [
+    'ChevronLeft',
+    <svg key={'ChevronLeft'} xmlns="http://www.w3.org/2000/svg" version="1.1" width="24" height="24" viewBox="0 0 24 24">
+      <path fill="currentColor" d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" />
+    </svg>
+  ],
+  [
+    'ChevronRight',
+    <svg key={'ChevronRight'} xmlns="http://www.w3.org/2000/svg" version="1.1" width="24" height="24" viewBox="0 0 24 24">
+      <path fill="currentColor" d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+    </svg>
+  ],
 ])
 
 interface Props {
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -57,7 +57,9 @@ const Home: NextPage = () => {
             00:00:00
           </div>
         </Progress>
-        <button onClick={startFasting}>Start Fast</button>
+        <div className={styles.action}>
+          <button onClick={startFasting}>Start Fast</button>
+        </div>
       </>
     : <>
         <Progress start={fasts.current.start} target={fasts.target}>
@@ -67,7 +69,9 @@ const Home: NextPage = () => {
           </div>
         </Progress>
         {status}
-        <button onClick={stopFasting}>End Fast</button>
+        <div className={styles.action}>
+          <button onClick={stopFasting}>End Fast</button>
+        </div>
       </>
 
   return <>
@@ -75,36 +79,50 @@ const Home: NextPage = () => {
       {progress}
 
       <div className={styles.report}>
+        <div className={styles.report_control}>
+          <button>
+            <Icon name={'ChevronLeft'}/>
+          </button>
+        </div>
         <div className={styles.report_month}>January 2022</div>
-        <div className={styles.report_days}>
-          <div className={styles.report_day}>
-            <div className={styles.report_number}>15</div>
-            <div className={styles.report_hours}></div>
+
+        <div className={styles.report_content}>
+          <div className={styles.report_days}>
+            <div className={styles.report_day}>
+              <div className={styles.report_number}>15</div>
+              <div className={styles.report_hours}></div>
+            </div>
+            <div className={styles.report_day}>
+              <div className={styles.report_number}>16</div>
+              <div className={styles.report_hours}></div>
+            </div>
+            <div className={styles.report_day}>
+              <div className={styles.report_number}>17</div>
+              <div className={styles.report_hours}></div>
+            </div>
+            <div className={styles.report_day}>
+              <div className={styles.report_number}>18</div>
+              <div className={styles.report_hours}></div>
+            </div>
+            <div className={styles.report_day}>
+              <div className={styles.report_number}>19</div>
+              <div className={styles.report_hours}></div>
+            </div>
+            <div className={styles.report_day}>
+              <div className={styles.report_number}>20</div>
+              <div className={styles.report_hours}></div>
+            </div>
+            <div className={styles.report_day}>
+              <div className={styles.report_number}>21</div>
+              <div className={styles.report_hours}></div>
+            </div>
           </div>
-          <div className={styles.report_day}>
-            <div className={styles.report_number}>16</div>
-            <div className={styles.report_hours}></div>
-          </div>
-          <div className={styles.report_day}>
-            <div className={styles.report_number}>17</div>
-            <div className={styles.report_hours}></div>
-          </div>
-          <div className={styles.report_day}>
-            <div className={styles.report_number}>18</div>
-            <div className={styles.report_hours}></div>
-          </div>
-          <div className={styles.report_day}>
-            <div className={styles.report_number}>19</div>
-            <div className={styles.report_hours}></div>
-          </div>
-          <div className={styles.report_day}>
-            <div className={styles.report_number}>20</div>
-            <div className={styles.report_hours}></div>
-          </div>
-          <div className={styles.report_day}>
-            <div className={styles.report_number}>21</div>
-            <div className={styles.report_hours}></div>
-          </div>
+        </div>
+
+        <div className={styles.report_control}>
+          <button>
+            <Icon name={'ChevronRight'}/>
+          </button>
         </div>
       </div>
     </div>
