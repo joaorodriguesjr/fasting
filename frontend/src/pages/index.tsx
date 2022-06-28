3b37833a910483f1063d82e20252e3fe08a4a89c
joaorodriguesjr <mail@joaorodriguesjr.com>
2022-06-28 07:39:55 -0300

    Add visual feedback for the value

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -95,31 +95,45 @@ const Home: NextPage = () => {
           <div className={styles.report_days}>
             <div className={styles.report_day}>
               <div className={styles.report_number}>15</div>
-              <div className={styles.report_hours}></div>
+              <div className={styles.report_hours}>
+                <div className={styles.report_value} style={{ height: '115px' }}></div>
+              </div>
             </div>
             <div className={styles.report_day}>
               <div className={styles.report_number}>16</div>
-              <div className={styles.report_hours}></div>
+              <div className={styles.report_hours}>
+                <div className={styles.report_value} style={{ height: '80px' }}></div>
+              </div>
             </div>
             <div className={styles.report_day}>
               <div className={styles.report_number}>17</div>
-              <div className={styles.report_hours}></div>
+              <div className={styles.report_hours}>
+                <div className={styles.report_value} style={{ height: '125px' }}></div>
+              </div>
             </div>
             <div className={styles.report_day}>
               <div className={styles.report_number}>18</div>
-              <div className={styles.report_hours}></div>
+              <div className={styles.report_hours}>
+                <div className={styles.report_value} style={{ height: '110px' }}></div>
+              </div>
             </div>
             <div className={styles.report_day}>
               <div className={styles.report_number}>19</div>
-              <div className={styles.report_hours}></div>
+              <div className={styles.report_hours}>
+                <div className={styles.report_value} style={{ height: '0px' }}></div>
+              </div>
             </div>
             <div className={styles.report_day}>
               <div className={styles.report_number}>20</div>
-              <div className={styles.report_hours}></div>
+              <div className={styles.report_hours}>
+                <div className={styles.report_value} style={{ height: '0px' }}></div>
+              </div>
             </div>
             <div className={styles.report_day}>
               <div className={styles.report_number}>21</div>
-              <div className={styles.report_hours}></div>
+              <div className={styles.report_hours}>
+                <div className={styles.report_value} style={{ height: '0px' }}></div>
+              </div>
             </div>
           </div>
         </div>
