7b8cb704ffaad34639a4d1bcc722e8e94bd87004
joaorodriguesjr <mail@joaorodriguesjr.com>
2022-07-02 08:26:08 -0300

    Add report component

---
+++ frontend/src/components/Report.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from 'react'
+import styles from './Report.module.css'
+
+interface Props {
+}
+
+const Report: FunctionComponent<Props> = () => {
+  return <></>
+}
+
+export default Report
