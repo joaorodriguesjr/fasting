958182f1cefafa686a696e3264d9a8d872b09841
joaorodriguesjr <mail@joaorodriguesjr.com>
2022-07-03 08:54:00 -0300

    Adjust size of progress circle

--- frontend/src/components/Progress.tsx
+++ frontend/src/components/Progress.tsx
@@ -14,12 +14,12 @@ const Progress: FunctionComponent<ProgressProps> = ({ start, target, children })
 
   const marker = (start === 0)
     ? <></>
-    : <circle className={styles.circle__01} strokeDashoffset={strokeDashoffset} cx="175" cy="175" r="150" strokeLinecap="round" />
+    : <circle className={styles.circle__01} strokeDashoffset={strokeDashoffset} cx="50%" cy="50%" r="115" strokeLinecap="round"/>
 
 
   return <div className={styles.container}>
-    <svg className={styles.svg} xmlns="http://www.w3.org/2000/svg" version="1.1" width="350px" height="350px">
-      <circle className={styles.circle__00} cx="175" cy="175" r="150" strokeLinecap="round" />
+    <svg className={styles.svg} viewBox="0 0 250 250">
+      <circle className={styles.circle__00} cx="50%" cy="50%" r="115" strokeLinecap="round"/>
       {marker}
     </svg>
     <div className={styles.children}>{children}</div>
