83086ec66c4f05bd1c8abf4741df5eb20aea3c36
joaorodriguesjr <mail@joaorodriguesjr.com>
2022-07-05 08:52:29 -0300

    Update component classes

--- frontend/src/components/Report.tsx
+++ frontend/src/components/Report.tsx
@@ -7,62 +7,62 @@ interface Props {
 
 const Report: FunctionComponent<Props> = () => {
   return <>
-    <div className={styles.report}>
-        <div className={styles.report_control}>
+    <div className={styles.container}>
+        <div className={styles.control}>
           <button>
             <Icon name={'ChevronLeft'}/>
           </button>
         </div>
-        <div className={styles.report_month}>January 2022</div>
+        <div className={styles.month}>January 2022</div>
 
-        <div className={styles.report_content}>
-          <div className={styles.report_days}>
-            <div className={styles.report_day}>
-              <div className={styles.report_number}>15</div>
-              <div className={styles.report_hours}>
-                <div className={styles.report_value} style={{ height: '115px' }}></div>
+        <div className={styles.content}>
+          <div className={styles.days}>
+            <div className={styles.day}>
+              <div className={styles.number}>15</div>
+              <div className={styles.hours}>
+                <div className={styles.value} style={{ height: '115px' }}></div>
               </div>
             </div>
-            <div className={styles.report_day}>
-              <div className={styles.report_number}>16</div>
-              <div className={styles.report_hours}>
-                <div className={styles.report_value} style={{ height: '80px' }}></div>
+            <div className={styles.day}>
+              <div className={styles.number}>16</div>
+              <div className={styles.hours}>
+                <div className={styles.value} style={{ height: '80px' }}></div>
               </div>
             </div>
-            <div className={styles.report_day}>
-              <div className={styles.report_number}>17</div>
-              <div className={styles.report_hours}>
-                <div className={styles.report_value} style={{ height: '125px' }}></div>
+            <div className={styles.day}>
+              <div className={styles.number}>17</div>
+              <div className={styles.hours}>
+                <div className={styles.value} style={{ height: '125px' }}></div>
               </div>
             </div>
-            <div className={styles.report_day}>
-              <div className={styles.report_number}>18</div>
-              <div className={styles.report_hours}>
-                <div className={styles.report_value} style={{ height: '110px' }}></div>
+            <div className={styles.day}>
+              <div className={styles.number}>18</div>
+              <div className={styles.hours}>
+                <div className={styles.value} style={{ height: '110px' }}></div>
               </div>
             </div>
-            <div className={styles.report_day}>
-              <div className={styles.report_number}>19</div>
-              <div className={styles.report_hours}>
-                <div className={styles.report_value} style={{ height: '0px' }}></div>
+            <div className={styles.day}>
+              <div className={styles.number}>19</div>
+              <div className={styles.hours}>
+                <div className={styles.value} style={{ height: '0px' }}></div>
               </div>
             </div>
-            <div className={styles.report_day}>
-              <div className={styles.report_number}>20</div>
-              <div className={styles.report_hours}>
-                <div className={styles.report_value} style={{ height: '0px' }}></div>
+            <div className={styles.day}>
+              <div className={styles.number}>20</div>
+              <div className={styles.hours}>
+                <div className={styles.value} style={{ height: '0px' }}></div>
               </div>
             </div>
-            <div className={styles.report_day}>
-              <div className={styles.report_number}>21</div>
-              <div className={styles.report_hours}>
-                <div className={styles.report_value} style={{ height: '0px' }}></div>
+            <div className={styles.day}>
+              <div className={styles.number}>21</div>
+              <div className={styles.hours}>
+                <div className={styles.value} style={{ height: '0px' }}></div>
               </div>
             </div>
           </div>
         </div>
 
-        <div className={styles.report_control}>
+        <div className={styles.control}>
           <button>
             <Icon name={'ChevronRight'}/>
           </button>
